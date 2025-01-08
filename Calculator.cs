2c8ea8263c915bd306fa31a5102483efd63f09b7
Kevin BJ <kevin.b.jonsson@hotmail.com>
2025-01-08 10:27:43 +0100

    first commit

---
+++ Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    public class Calculator
+    {
+        public int Add(int a, int b)
+        { return a + b; }
+
+        public int Subtract (int a, int b)
+        { return a - b; }
+
+        public int Multiply (int a, int b)
+        { return 0; }
+
+        public int Divide (int a, int b)
+        {return 0; }
+    }
+}
---
+++ CalculatorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace calc
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            //given
+            Calculator calc = new();
+            int a = 5;
+            int b = 2;
+            int expectedResult = 7;
+
+            //when
+            int result = calc.Add(a, b);
+
+            //then
+            Assert.Equal(expectedResult, result);
+
+        }
+    }
+}
